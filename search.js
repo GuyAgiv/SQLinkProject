ed9dfcbf2c024dd4b8edc646c8af583bd4f573b1
GuyAgiv <guyagiv@Gmail.com>
2018-10-16 19:20:23 +0300

    second commit

--- search.js
+++ search.js
@@ -2,53 +2,61 @@
 * ASSIGNED BY : GUY AGIV
 * */
 
-const path = require('path'), fs = require('fs');
-
+const path = require('path');
+const fs = require('fs');
+const helpMessageText = "USAGE: node search [EXT] [TEXT]";
+const fileNotFound = "No file was found";
 
 if(process.argv.length !== 4)
 {
-    console.log("USAGE: node search [EXT] [TEXT]")
+    console.log(helpMessageText);
 }
 
 else
 {
-    let results;
     const requiredExtension = process.argv[2];
     const requiredWord = process.argv[3];
-    const fileNotFound = "No file was found";
 
-    results = ExtractRelevantFilesByExtension(__dirname, requiredWord, requiredExtension);
+    result = ExtractRelevantFilesByExtension(__dirname, requiredWord, requiredExtension);
 
-    results.length === 0 ? console.log(fileNotFound) : console.log(results);
+    if(!result)
+    {
+        console.log(fileNotFound);
+    }
 }
 
+/**
+ * @return {boolean}
+ */
 function ExtractRelevantFilesByExtension(directory, word, extension)
 {
-    let filesList = [];
-    let desiredFilesList = [];
+    let isMatchFound = false;
 
-
-    // Recursive function , finding all files with the required extension (including subdirectories)
-    function SearchRecursivelyDeeplyInDirectory(directory, extension)
+    // Recursive function , finding all files with the required extension and  (including subdirectories)
+    function SearchRecursivelyDeeplyInDirectory(directory)
     {
         fs.readdirSync(directory).forEach(object => {
 
             const objectFullPath = path.join(directory, object);
-
             const stat = fs.lstatSync(objectFullPath);
 
+            // The recursive step
             if(stat.isDirectory())
             {
-                SearchRecursivelyDeeplyInDirectory(objectFullPath,extension);
+                SearchRecursivelyDeeplyInDirectory(objectFullPath);
             }
             else
             {
+                // The validation stage , looking if current file is fit for our requirements
                 if(path.extname(objectFullPath) === "." + extension)
                 {
-                    filesList.push(objectFullPath);
+                    const fileContent = fs.readFileSync(objectFullPath);
+                    if(fileContent.includes(word))
+                    {
+                        isMatchFound = true;
+                        console.log(objectFullPath);
+                    }
                 }
-
-
             }
         })
     }
@@ -56,16 +64,5 @@ function ExtractRelevantFilesByExtension(directory, word, extension)
     // Calling the internal function
     SearchRecursivelyDeeplyInDirectory(directory, extension);
 
-    if(filesList.length !== 0)
-    {
-        // Iterating the filesList which includes only the desired extension we looked for through lambda expression
-        filesList.forEach(file => {
-            const fileContent = fs.readFileSync(file);
-            if(fileContent.includes(word)) {
-                desiredFilesList.push(file);
-            }
-        });
-    }
-
-    return desiredFilesList;
+    return isMatchFound;
 }
