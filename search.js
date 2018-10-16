f035151d1fc329105725ab9320814bec12c7c860
GuyAgiv <guyagiv@Gmail.com>
2018-10-16 11:59:40 +0300

    first commit

---
+++ search.js
@@ -0,0 +1,73 @@
+/*
+* ASSIGNED BY : GUY AGIV
+* */
+
+const path = require('path'), fs = require('fs');
+
+
+if(process.argv.length !== 4)
+{
+    console.log("USAGE: node search [EXT] [TEXT]")
+}
+
+else
+{
+    let results;
+    const requiredExtension = process.argv[2];
+    const requiredWord = process.argv[3];
+    const fileNotFound = "No file was found";
+
+    results = ExtractRelevantFilesByExtension(__dirname, requiredWord, requiredExtension);
+
+    results.length === 0 ? console.log(fileNotFound) : console.log(results);
+}
+
+function ExtractRelevantFilesByExtension(directory, word, extension)
+{
+    let filesList = [];
+    let desiredFilesList = [];
+
+
+    // Recursive function , finding all files with the required extension (including subdirectories)
+    function SearchRecursivelyDeeplyInDirectory(directory, extension)
+    {
+        fs.readdirSync(directory).forEach(object => {
+
+            const objectFullPath = path.join(directory, object);
+
+            const stat = fs.lstatSync(objectFullPath);
+
+            if(stat.isDirectory())
+            {
+                SearchRecursivelyDeeplyInDirectory(objectFullPath);
+            }
+            else
+            {
+                if(path.extname(objectFullPath).split('.').pop() === extension)
+                {
+
+                    filesList.push(objectFullPath);
+                }
+
+
+            }
+        })
+    }
+
+    // Calling the internal function
+    SearchRecursivelyDeeplyInDirectory(directory, extension);
+
+    if(filesList.length !== 0)
+    {
+        // Iterating the filesList which includes only the desired extension we looked for through lambda expression
+        filesList.forEach(file => {
+            const fileContent = fs.readFileSync(file);
+
+            if(fileContent.includes(word)) {
+                desiredFilesList.push(file);
+            }
+        });
+    }
+
+    return desiredFilesList;
+}
